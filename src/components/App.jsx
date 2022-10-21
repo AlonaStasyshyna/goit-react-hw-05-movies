4f1fe3d0aaa319fd50ed8a6ebb40ff42e390e67c
AlonaStasyshyna <alena.stasishyna28@gmail.com>
2022-10-21 12:51:09 +0300

    Add start settings

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,64 +1,16 @@
-import { useState } from 'react';
-import { Section } from './Section/Section';
-import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
-import { Statistics } from './Statistics/Statistics';
-import { Notification } from './Notification/Notification';
-
 export const App = () => {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-
-  const onLeaveFeedback = e => {
-    const option = e.target.name;
-
-    switch (option) {
-      case 'good':
-        setGood(prevState => prevState + 1);
-        break;
-      case 'neutral':
-        setNeutral(prevState => prevState + 1);
-        break;
-      case 'bad':
-        setBad(prevState => prevState + 1);
-        break;
-      default:
-        return;
-    }
-  };
-
-  const countTotalFeedback = () => good + neutral + bad;
-
-  const countPositiveFeedbackPercentage = () => {
-    if (!good) {
-      return 0;
-    }
-
-    return Math.round((good / (good + neutral + bad)) * 100);
-  };
-
   return (
-    <>
-      <Section title="Please leave feedback">
-        <FeedbackOptions
-          options={{ good, neutral, bad }}
-          onLeaveFeedback={onLeaveFeedback}
-        />
-      </Section>
-
-      <Section title="Statistics">
-        {countTotalFeedback() ? (
-          <Statistics
-            good={good}
-            neutral={neutral}
-            bad={bad}
-            total={countTotalFeedback()}
-            positivePercentage={countPositiveFeedbackPercentage()}
-          />
-        ) : (
-          <Notification message="There is no feedback" />
-        )}
-      </Section>
-    </>
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      React homework template
+    </div>
   );
 };
